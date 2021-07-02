7a5b1a700ed113c50ac7a2853474003c33a0deb8
capncruncky <cybercruncky@gmail.com>
2021-07-02 12:09:40 -0400

    update to pointer_01.cpp - added additional text and array cycle code...

--- pointer_01.cpp
+++ pointer_01.cpp
@@ -8,6 +8,8 @@ int main()
     int* ptr_number = &number;
 
     std::cout << "*********************************************************" << std::endl;
+    std::cout << "pointer_01.cpp by capncruncky - github.com/capncruncky" << std::endl;
+    std::cout << "\n";
     std::cout << "We've stored the value " << number << " into the 'number' variable..." << std::endl;
     std::cout << "We've also referenced that variable with a pointer, ptr_number..." << std::endl;
     std::cout << "\tDereferencing ptr_number: " << *ptr_number << std::endl;
@@ -25,7 +27,23 @@ int main()
     std::cout << "*********************************************************" << std::endl;
     std::cout << "Here we will loop through the array and ID each element's " << std::endl;
     std::cout << "memory location as well as its value..." << std::endl;
+    std::cout << "\n";
 
+    //Here we cycle through the array and display each element...0
+    for (int i = 0; i < 26; i++)
+    {
+        //std::cout << "Memset value: " << buffer[i];
+        std::cout << "\tmemLoc: " << &buffer[i];
+        std::cout << "\tvalue: " << *buffer << std::endl;
+    }
+
+    std::cout << "\n";
+    std::cout << "Above, we can see that all memory values have been set to '0'" << std::endl;
+    std::cout << "Next, we will cycle through the array and increase each value by 1 byte" << std::endl;
+    std::cout << "\n";
+
+    //Here we cycle through the array and use the *dereference to alter the value
+    //of the element - we increase the value by one byte, or sizeof(char)
     for (int i = 0; i < 26; i++)
     {
         //std::cout << "Memset value: " << buffer[i];
